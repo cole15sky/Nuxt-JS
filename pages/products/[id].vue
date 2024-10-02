c845a8f8455fa8d6657b1e27807a3f670a40fefa
Sky Blue <dawadiaakash@gmail.com>
2024-10-02 11:38:53 +0545

    Nuxt(Route): playing with route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+<p> Product details for {{ id }}</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero quibusdam sint ipsum corrupti fugit delectus asperiores iste nihil cum rerum eligendi debitis nisi provident architecto incidunt, corporis consectetur, voluptas amet.</p>
+    </div>
+</template>
+
+<script setup>
+const {id }=useRoute().params
+</script>
+
+<style scoped>
+
+</style>
